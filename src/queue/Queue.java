77fc019944ea422f71d87b926a8f608a33ddd5f7
zhengdayu97 <zc_work@outlook.com>
2020-02-15 00:44:10 +0800

    定义Queue接口

--- src/queue/Queue.java
+++ src/queue/Queue.java
@@ -3,12 +3,12 @@ package queue;
 /**
  * @Author: ZhengCheng
  * @Date: created in 23:52  2020/2/10
- * @Annotation:
+ * @Annotation:队列接口
  */
 public interface Queue<E> {
-    void enqueue(E e);
-    E dequeue();
-    E getFront();
-    int getSize();
-    boolean isEmpty();
+    void enqueue(E e); //进队操作
+    E dequeue();  //出队操作
+    E getFront();  //获取队首元素
+    int getSize();  //获取队列元素总数
+    boolean isEmpty();  //判断队列是否为空
 }
