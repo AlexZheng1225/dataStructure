546c2e3762ca1ae82198976d675f98be4cc3389a
AlexZheng97 <zc_work@outlook.com>
2020-10-21 16:34:53 +0800

    AVL Tree的实现以及AVL Tree实现Map映射

---
+++ src/AVLTree/BSTMap.java
@@ -0,0 +1,216 @@
+package AVLTree;
+
+import Map.Map;
+import Set.FileOperation;
+
+import java.util.ArrayList;
+
+/**
+ * @Author: ZhengCheng
+ * @Date: created in 14:56  2020/3/1
+ * @Annotation: 基于二分搜素树实现的映射Map 需要对原BST进行修改
+ */
+//必须是可以比较的
+public class BSTMap<K extends Comparable<K>,V> implements Map<K,V> {
+
+    class Node{
+        public K key;
+        public V value;
+        public Node left;
+        public Node right;
+
+        public Node(K key,V value){
+            this.key = key;
+            this.value = value;
+            left = null;
+            right = null;
+        }
+    }
+
+    private int size;
+    private Node root;
+
+    public BSTMap(){
+        root = null;
+        size=0;
+    }
+
+    @Override
+    public void add(K key, V value) {
+        root = add(root,key,value);
+    }
+
+    private Node add(Node node, K key, V value) {
+        if(node==null){
+            size++;
+            return new Node(key,value);
+        }
+
+        if(key.compareTo(node.key)<0){
+            node.left = add(node.left,key,value);
+        }else if(key.compareTo(node.key)>0){
+            node.right = add(node.right,key,value);
+        }else{ //key==node.key
+            //当添加的key映射中已经有时
+            node.value = value;
+        }
+        return node;
+    }
+
+    //返回以node为根节点的二分搜索树中，key所在的节点
+    private Node getNode(Node node,K key){
+        if(node==null){
+            return null;
+        }
+        if(key.compareTo(node.key)==0){
+            return node;
+        }else if(key.compareTo(node.key)<0){
+            return getNode(node.left,key);
+        }else{
+            return getNode(node.right,key);
+        }
+    }
+
+    @Override
+    public V remove(K key) {
+        Node node = getNode(root,key);
+        if(node==null){
+            return null;
+        }
+        root = remove(root,key);
+        return node.value;
+    }
+
+    private Node remove(Node node,K key){
+        if(node==null){
+            return null;
+        }
+
+        if(key.compareTo(node.key)<0){
+            node.left = remove(node.left,key);
+        }else if(key.compareTo(node.key)>0){
+            node.right = remove(node.right,key);
+        }else{ //key==node.key
+
+            //待删除节点左子树为空的情况
+            if(node.left==null){
+                Node rightNode = node.right;
+                node.right = null;
+                size--;
+                return rightNode;
+            }
+            //待删除节点右子树为空的情况
+            if(node.right == null){
+                Node leftNode = node.left;
+                node.left = null;
+                size--;
+                return leftNode;
+            }
+
+            //待删除节点左右子树都不为空
+            //找到比待删除节点要大的最小的节点，即待删除右子树最小的节点
+            //用此节点顶替待删除结点
+            Node successor = minimum(node.right);  //找到最小值
+            successor.right = removeMin(node.right);  //将右子树的最小值删除并返回删除后的结果
+            successor.left = node.left;
+            //在此处不用维护size，因为在调用removeMin方法的时候已经对size进行了处理
+            node.left = node.right = null;
+            return successor;
+        }
+        return node;
+
+    }
+
+    //返回node为根的二分搜索树的最小值所在的结点
+    private Node minimum(Node node) {
+        if(node.left==null){
+            return node;
+        }
+        return minimum(node.left);
+    }
+
+    //删除掉以node为根的二分搜索树中的最小节点
+    //返回删除结点后二分搜索树的根
+    private Node removeMin(Node node) {
+        if(node.left==null){
+            Node rightNode = node.right;
+            node.right = null;
+            size--;
+            return rightNode;
+        }
+        node.left = removeMin(node.left);  //右节点移动到了被删元素的位置上
+        return node;
+    }
+
+    @Override
+    public boolean contains(K key) {
+        return getNode(root,key)!=null;
+    }
+
+
+    @Override
+    public V get(K key) {
+        Node node = getNode(root,key);
+        if(node==null){
+            return null;
+        }
+        return node.value;
+    }
+
+    @Override
+    public void set(K key, V newValue) {
+        Node node = getNode(root,key);
+        if(node==null){
+            throw new IllegalArgumentException(key+"does't exist!");
+        }
+        node.value = newValue;
+    }
+
+
+    @Override
+    public int getSize() {
+        return size;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println("Pride and Prejudice");
+        ArrayList<String> word1 = new ArrayList<>();
+        if (FileOperation.readFile("E:\\JavaWeb\\Algorithms\\dataStructure\\src\\pride-and-prejudice.txt", word1)) {
+            System.out.println("Total words:" + word1.size());
+            BSTMap<String,Integer> map = new BSTMap<String,Integer>();
+            for (String word : word1) {
+                if(map.contains(word)){
+                    map.set(word,map.get(word)+1);
+                }else{
+                    map.add(word,1);
+                }
+            }
+            System.out.println("Total different words:" + map.getSize());
+            System.out.println("Frequency of pride:"+map.get("pride"));
+        }
+
+        System.out.println();
+
+        System.out.println("a-tale-of-two-cities");
+        ArrayList<String> word2 = new ArrayList<>();
+        if (FileOperation.readFile("E:\\JavaWeb\\Algorithms\\dataStructure\\src\\a-tale-of-two-cities.txt", word2)) {
+            System.out.println("Total words:" + word2.size());
+            BSTMap<String,Integer> map = new BSTMap<String,Integer>();
+            for (String word : word2) {
+                if(map.contains(word)){
+                    map.set(word,map.get(word)+1);
+                }else{
+                    map.add(word,1);
+                }
+            }
+            System.out.println("Total different words:" + map.getSize());
+            System.out.println("Frequency of cities:"+map.get("cities"));
+        }
+    }
+}
---
+++ src/AVLTree/Main.java
@@ -0,0 +1,64 @@
+package AVLTree;
+
+import Set.BST;
+import Set.FileOperation;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+/**
+ * @Author Alex Zheng
+ * @Date 2020/10/21 13:01
+ * @Annotation
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Pride and Prejudice");
+        ArrayList<String> words = new ArrayList<>();
+        if (FileOperation.readFile("E:\\JavaWeb\\Algorithms\\dataStructure\\src\\pride-and-prejudice.txt", words)) {
+            System.out.println("Total words:" + words.size());
+
+            //使得BST添加进去变成链表
+            Collections.sort(words);
+
+            //test BST
+            long startTime = System.nanoTime();
+
+            BSTMap<String, Integer> bst = new BSTMap<String, Integer>();
+            for (String word : words) {
+                if (bst.contains(word)) {
+                    bst.set(word, bst.get(word) + 1);
+                } else {
+                    bst.add(word, 1);
+                }
+            }
+
+            for (String word : words) {
+                bst.contains(word);
+            }
+
+            long endTime = System.nanoTime();
+            double time = (endTime - startTime) / 1000000000.0;
+            System.out.println("Test BST Time: " + time + "s");
+
+            //Test AVL Tree
+            startTime = System.nanoTime();
+            AVLTree<String, Integer> avl = new AVLTree<String, Integer>();
+            for (String word : words) {
+                if (avl.contains(word)) {
+                    avl.set(word, avl.get(word) + 1);
+                } else {
+                    avl.add(word, 1);
+                }
+            }
+            for (String word : words) {
+                avl.contains(word);
+            }
+            endTime = System.nanoTime();
+            time = (endTime - startTime) / 1000000000.0;
+            System.out.println("Test AVL Tree Time: " + time + "s");
+        }
+
+    }
+}
